4989a1a2e1a6d1212097d3a9531a548602455a2c
sdias-code <silviodias.ms@gmail.com>
2024-01-01 10:58:02 -0400

    Desafio concluído by sdias

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("9999-5555", "NK-G60", "352906112950151", 512);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("JW Library");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("8888-4444", "Iphone 11", "352906117758955", 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("JW Language");
